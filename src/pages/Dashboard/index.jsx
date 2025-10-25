cd0b6dcef386492ed8100c81b623a5ee16f787b2
sudevss <sudevss2002@gmail.com>
2025-10-25 10:42:28 +0530

    Update index.jsx

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -36,7 +36,7 @@ import EnhancedPieChart from "@components/charts/EnhancedPieChart";
 import EnhancedStackedBarChart from "@components/charts/EnhancedStackedBarChart";
 import StyledButton from "@components/StyledButton";
 import AppointmentsTable from "@/ReusableComponents/AppointmentsTable";
-import SelectWithLabel from '@/components/inputs/SelectWithLabel.jsx';
+import SelectWithLabel from '@/components/inputs/SelectWithLabel';
 import AddOrEditBooking from "@/ReusableComponents/AddOrEditBooking";
 import PageLoader from "@pages/PageLoader";
 import AlertSnackbar from "@components/AlertSnackbar";
@@ -421,4 +421,5 @@ function DashboardPage() {
 }
 
 export default DashboardPage;
+
 
